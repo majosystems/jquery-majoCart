dc5b5b80a7a67ea6daa5499b31ecda5e35c2a9ff
takashi.akagi <nisyu@mac.com>
2015-03-20 17:44:56 +0900

    サンプルスタイル追加

--- jQuery.majoCart.js
+++ jQuery.majoCart.js
@@ -14,37 +14,42 @@
     };
     var settings = $.extend(defaults, options);
 
-    var cart_cals_line = $(cart_view.find('.'+settings.cals)[0]);
-    var cart_item_line = $(cart_view.find('.'+settings.item)[0]);
+    // セレクタの生成helper
+    var ss = function(selecter_str){
+      return('.' + selecter_str);
+    }
+
+    var cart_cals_line = $(cart_view.find(ss(settings.cals))[0]);
+    var cart_item_line = $(cart_view.find(ss(settings.item))[0]);
     var cart_cals_base = cart_item_line.parent();
 
     var cart_items = [];
 
     var show_cart = function(items){
-      $(cart_view.find('.'+settings.cals)).remove();
-      $(cart_view.find('.'+settings.item)).remove();
+      $(cart_view.find(ss(settings.cals))).remove();
+      $(cart_view.find(ss(settings.item))).remove();
       var total = 0;
       for(var i = 0; i < items.length; i++){
         var this_item = cart_item_line.clone();
-        this_item.find('.'+settings.name).text(items[i].name);
-        this_item.find('.'+settings.code).text(items[i].code);
-        this_item.find('.'+settings.price).text(items[i].price);
-        this_item.find('.'+settings.quantity).text(1);
+        this_item.find(ss(settings.name)).text(items[i].name);
+        this_item.find(ss(settings.code)).text(items[i].code);
+        this_item.find(ss(settings.price)).text(items[i].price);
+        this_item.find(ss(settings.quantity)).text(1);
         total += parseInt(items[i].price);
         cart_cals_base.append(this_item);
       }
       this_item = cart_cals_line.clone();
-      this_item.find('.'+settings.total).text(total);
+      this_item.find(ss(settings.total)).text(total);
       cart_cals_base.append(this_item);
     }
 
-    $('.'+settings.item).each(function(){
+    $(ss(settings.item)).each(function(){
         var item_view = $(this);
-        item_view.find('.'+settings.add_to_cart).click(function(){
+        item_view.find(ss(settings.add_to_cart)).click(function(){
           cart_items.push({
-            name: item_view.find('.'+settings.name).text(),
-            code: item_view.find('.'+settings.code).text(),
-            price: item_view.find('.'+settings.price).text()
+            name: item_view.find(ss(settings.name)).text(),
+            code: item_view.find(ss(settings.code)).text(),
+            price: item_view.find(ss(settings.price)).text()
           });
           show_cart(cart_items);
         });
